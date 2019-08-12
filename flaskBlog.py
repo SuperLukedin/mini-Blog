7bbbb5811c1ccd69bcd7f36cf1a6ed356a2888ff
SuperLukedin <yp54@njit.edu>
2019-08-11 22:22:38 -0400

    mini blog initial commit

---
+++ flaskBlog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Yuchen',
+        'title': 'Sunday Post',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Sara',
+        'title': 'title post from sara',
+        'content': 'First post content from sara',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
